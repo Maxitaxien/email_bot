6e86bad77ee2f6e8ef32dddf9dad492c5bc4764c
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2025-09-10 16:38:58 +0200

    Completed most of main logic

--- main.py
+++ main.py
@@ -1,66 +1,79 @@
 import smtplib, ssl
-from pathlib import Path
+from dataloaders import *
+from email_helpers import *
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-port = 465 # ssl port
-password_path = Path('secrets') / 'passwd'
-sender_email_path = Path('secrets') / 'sender'
-
-if sender_email_path.exists():
-    sender_email = sender_email_path.read_text()
-else:
-    sender_email = input("Enter email to send from: ")
-
-if password_path.exists():
-    password = password_path.read_text()
-else:
-    password = input("Enter password: ")
-
-
-# By default, just send to the same email with the tag pythonbot
-receiver_email = sender_email[:sender_email.find('@')] + "+pythonbot" + sender_email[sender_email.find('@'):]
-
-
-
-message = MIMEMultipart("alternative")
-message["Subject"] = "Multipart test"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-text = """\
-This is a test using the MIME message type.
-The nice thing about this is that we can send both
-plain text and html. 
-See my website:
-https://maxitaxien.github.io/portfolioWebsite/
-"""
-
-html = """\
-<html>
-    <body>
-        <p>This is a test using the MIME message type.<br>
-        The nice thing about this is that we can send both<br>
-        plain text and html.<br>
-        See my website:<br>
-        <a href="https://maxitaxien.github.io/portfolioWebsite/">Website </a><br>
-        </p>
-    </body>
-</html>
-"""
-
-# Turn iunto MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
-
-# Attach to multipartmessage, last part will try to be rendered first
-message.attach(part1)
-message.attach(part2)
+# Constants:
+# SSL Port
+port = 465
 
 # Create secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, password)
+def main(template_name: str, subject: str = '', data_name: str = '', columns: list = [], recipients: list = []) -> None:
+    '''
+    Main function for email handler.
+    Subject can be given either as a string if the same for all emails, or can be supplied in data if it should be personalized.
 
-    server.sendmail(sender_email, receiver_email, message.as_string())
+    If no template arguments are needed, the list of recipients is still needed. The list of recipients
+    can be passed as a datafile using a file referred to with data_name, or in this case the receipients arg can be used.
+
+    If any template arguments { } exist in the template, the program expects data to fill these in.
+    This should be given through a file located in data/ using the data_name argument.
+
+
+    :param template_name str: Name of template to use for filling in text.
+    :param subject str: Subject line, can be supplied as single string if the same for all emails.
+    :param data_name str: Data name to look for under the data/ dir. 
+    :param columns list: A list of specific columns from the data to use.
+    :param recipients list: When not parameterizing, can be used to pass a list of recipient emails.
+    '''
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        sender_email = login(server)
+
+        if not sender_email:
+            logger.info('Exiting program run.')
+            return
+
+        # Setup email sending tasks
+        tasks = []
+
+        # Load in templates to use
+        txt_template, html_template = load_templates(template_name)
+        needs_format = (txt_template.find('{') and txt_template.find('}')) or (html_template.find('{') and html_template.find('}'))
+
+        # Load in data for parameter filling
+        if data_name:
+            data = extract_data(data_name, columns)
+            recipients, col = find_email_col(data)
+        
+
+        # If formatting:
+        if needs_format:
+            pass
+
+        # If no email recipients were found, return to sender
+        if not recipients and not data_name:
+            receiver_email = sender_email[:sender_email.find('@')] + "+pythonbot" + sender_email[sender_email.find('@'):]
+
+
+        # If not formatting, send email as-is
+        for recipient in recipients:
+            message = MIMEMultipart("alternative")
+            message["Subject"] = subject # make this inferrable from data
+            message["From"] = sender_email
+            message["To"] = receiver_email
+
+            # Convert to MIMETexxt
+            text_part = MIMEText(txt_template, "plain")
+            html_part = MIMEText(html_template, "html")
+
+            # Attach, trying to render last part first
+            message.attach(text_part)
+            message.attach(html_part)
+
+            server.sendmail(sender_email, receiver_email, message.as_string())
+
+if __name__ == '__main__':
+    main(template_name = 'example', subject = 'Book status')
